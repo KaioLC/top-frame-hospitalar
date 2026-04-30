4754293ad903889db75108ec455830ca6eb42962
KaioLC <kaiolopesc00@gmail.com>
2026-04-30 02:36:40 -0300

    navegacao entre paginas estruturada

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Telemetria from './pages/Telemetria'
 import CadastroMissoes from './pages/CadastroMissoes'
-
+import './styles/App.css'
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Telemetria />} />
+        <Route path="/missoes" element={<CadastroMissoes />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,13 @@ import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className="abas">
+    <nav className="navbar">
       <NavLink to="/" className={({ isActive }) => isActive ? 'aba aba-ativa' : 'aba'}>
         Telemetria
       </NavLink>
-
+      <NavLink to="/missoes" className={({ isActive }) => isActive ? 'aba aba-ativa' : 'aba'}>
+        Missões
+      </NavLink>
     </nav>
   )
 }
